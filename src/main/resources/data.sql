09b04a97a3180330db8df0e6097a14ebded05323
Hendi Santika <hendisantika@gmail.com>
2019-12-04 06:15:27 +0700

    #16 - Modify sql script into flyway

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO customer(name,email,created_date)VALUES('hendisantika','111@yahoo.com', '2017-03-24');
-INSERT INTO customer(name,email,created_date)VALUES('naruto','222@yahoo.com', '2017-03-24');
-INSERT INTO customer(name,email,created_date)VALUES('kakashi','333@yahoo.com', '2017-03-24');
--- src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS customer;
-CREATE TABLE customer (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(100) NOT NULL,
-  email VARCHAR(100) NOT NULL,
-  created_date DATE NOT NULL,
-  PRIMARY KEY (id));
